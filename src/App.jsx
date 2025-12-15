8c2f2cec2032365c03be9fad1cf6a3351442a779
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-15 11:43:03 +0530

    feat: implement routing in App component with Routes and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-
 import HomeLayout from './Pages/Landing/HomeHandler'
+import AboutEcom from './Pages/Project/AboutEcom'
+import NotFound from './Pages/NotFound/NotFound'
+import Blog from './Pages/Blog/Blog'
+import LandingProject from './Pages/Project/LandingProject'
 
 function App() {
   return (
     <>
-      <HomeLayout />
+      <Routes>
+        <Route index element={<HomeLayout />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="/project" element={<LandingProject />}>
+          <Route path="1" element={<AboutEcom />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
