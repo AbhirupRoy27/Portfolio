6f7a6efc84563031933ad01d39bc6f82391f0e8c
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-17 11:16:44 +0530

    feat: restructure routing in App component and update navigation links in the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import HomeLayout from './Pages/Landing/HomeHandler'
+import HomeLayout, { HomeSkeliton } from './Pages/Landing/HomeHandler'
 import AboutEcom from './Pages/Project/AboutEcom'
 import NotFound from './Pages/NotFound/NotFound'
 import Blog from './Pages/Blog/Blog'
 import LandingProject from './Pages/Project/LandingProject'
+import ProjectLanding from './Pages/Project/ProjectLanding'
+import AboutWorkSync from './Pages/Project/AboutWorkSync'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route index element={<HomeLayout />} />
-        <Route path="blog" element={<Blog />} />
-        <Route path="/project" element={<LandingProject />}>
-          <Route path="1" element={<AboutEcom />} />
+        <Route path="/" element={<HomeLayout />}>
+          <Route index element={<HomeSkeliton />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="project" element={<LandingProject />}>
+            <Route index element={<ProjectLanding />} />
+            <Route path="1" element={<AboutEcom />} />
+            <Route path="2" element={<AboutWorkSync />} />
+          </Route>
         </Route>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ActiveSection from './Contexts/activeSection.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ActiveSection>
+      <App />
+    </ActiveSection>
   </BrowserRouter>
 )
