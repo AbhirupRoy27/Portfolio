283be8d7767c8f9129e5ef9ece5aa5cfcfe09f55
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-02-14 19:27:03 +0530

    feat: enhance ContactForm component with state management and message reset functionality

--- src/Pages/Contact/components/ContactForm.jsx
+++ src/Pages/Contact/components/ContactForm.jsx
@@ -1,23 +1,57 @@
 import { LogIn } from 'lucide-react'
-import React from 'react'
+import { useState } from 'react'
 
 function ContactForm() {
+  const [message, setMessage] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
   return (
-    <form className="flex flex-col w-full gap-6 mt-8 text-lg md:max-w-[654px] text-white">
+    <form
+      className="flex flex-col w-full gap-6 mt-8 text-lg md:max-w-[654px] text-white"
+      onSubmit={(e) => handleMessage(e, setMessage, message)}
+    >
       <input
         placeholder="Name"
         autoFocus={false}
         className="py-2 focus:outline-none focus:bg-gray-50/10 focus:border-white focus:pl-2 border-b-1 border-white/70"
+        required
+        value={message.name}
+        onChange={(e) =>
+          setMessage((prev) => ({
+            ...prev,
+            name: e.target.value,
+          }))
+        }
       />
       <input
+        type="email"
         placeholder="Email"
         autoFocus={false}
         className="py-3 focus:outline-none focus:bg-gray-50/10 focus:border-white focus:pl-2 border-b-1 border-white/70"
+        required
+        value={message.email}
+        onChange={(e) =>
+          setMessage((prev) => ({
+            ...prev,
+            email: e.target.value,
+          }))
+        }
       />
       <input
+        type="text"
         placeholder="Message"
         autoFocus={false}
         className="py-3 focus:outline-none focus:bg-gray-50/10 focus:border-white focus:pl-2 border-b-1 border-white/70"
+        required
+        value={message.message}
+        onChange={(e) =>
+          setMessage((prev) => ({
+            ...prev,
+            message: e.target.value,
+          }))
+        }
       />
       <button
         type="submit"
@@ -30,3 +64,21 @@ function ContactForm() {
 }
 
 export default ContactForm
+
+function handleMessage(e, setMessage, message) {
+  e.preventDefault()
+  alert(
+    'Message sent\n' +
+      'name: ' +
+      message.name +
+      '\nEmail: ' +
+      message.email +
+      '\nMessage: ' +
+      message.message,
+  )
+  return setMessage({
+    name: '',
+    email: '',
+    message: '',
+  })
+}
